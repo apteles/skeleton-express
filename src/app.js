1975ac4542ac6b82e5694c2d133a9158759178f1
Andre Teles <andre.telestp@gmail.com>
2020-06-30 15:18:34 -0300

    applied eslint fix

--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
-import 'dotenv/config'
-import express from 'express'
-import Youch from 'youch'
-import 'express-async-errors'
-import routes from './app/routes'
+import 'dotenv/config';
+import express from 'express';
+import Youch from 'youch';
+import 'express-async-errors';
+import routes from './app/routes';
 
 class App {
   constructor() {
-    this.server = express()
+    this.server = express();
 
-    this.middlewares()
-    this.routes()
-    this.exceptionHandler()
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
   }
 
-  middlewares() { }
+  middlewares() {}
 
   routes() {
-    this.server.use(routes)
+    this.server.use(routes);
   }
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
-        const errors = await new Youch(err, req).toJSON()
-        return res.status(500).json(errors)
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
       }
-
-      return res.status(500).json({ error: 'Server Internal error' })
-
-    })
+      return res.status(500).json({ error: 'Server Internal error' });
+    });
   }
 }
 
-export default new App().server
+export default new App().server;
